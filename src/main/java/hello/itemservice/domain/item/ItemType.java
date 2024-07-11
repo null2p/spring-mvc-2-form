f5f7144cb62ca437a782cab59174cb1276d20bc1
nuri park <nuri_park2@tmax.co.kr>
2024-07-11 19:31:10 +0900

    [feat] 타임리프 제공 라디오 버튼 도입 및 라디오 버튼을 이용한 상품 종류 선택 기능 추가

--- src/main/java/hello/itemservice/domain/item/ItemType.java
+++ src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -1,9 +1,12 @@
 package hello.itemservice.domain.item;
 
+import lombok.Getter;
+
 public enum ItemType {
     BOOK("도서"), FOOD("음식"), ETC("기타");
 
-    private String description;
+    @Getter
+    private final String description;
 
     ItemType(String description) {
         this.description = description;
